ae6cc104754448c1969cc4c5b265f97c134eff61
coredelle <westga09@gmail.com>
2020-05-09 22:01:55 -0400

    change output to farenheit

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,10 @@
 const request = require('postman-request');
 
-const url = "http://api.weatherstack.com/current?access_key=852f24ff0f88bb9de21b4e0c7d6032ca&query=37.8267,-122.4233";
+const url = "http://api.weatherstack.com/current?access_key=852f24ff0f88bb9de21b4e0c7d6032ca&query=37.8267,-122.4233&units=f";
 
-request({url}, (err, resp) => {
-    const data = JSON.parse(resp.body);
-    console.log(data.current);
+request({url, json: true}, (err, resp) => {
+    // console.log(resp.body.current);
+    const degrees = resp.body.current.temperature;
+    const rain = resp.body.current.precip;
+    console.log(`It is currently ${degrees} degrees out. There is ${rain}% chance of rain!`)
 });
