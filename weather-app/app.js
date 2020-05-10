e6dcda51784ba73dca1fe846f765f71192d82406
coredelle <westga09@gmail.com>
2020-05-09 21:45:57 -0400

    narrow down call

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,5 +3,6 @@ const request = require('postman-request');
 const url = "http://api.weatherstack.com/current?access_key=852f24ff0f88bb9de21b4e0c7d6032ca&query=37.8267,-122.4233";
 
 request({url}, (err, resp) => {
-    console.log(resp);
+    const data = JSON.parse(resp.body);
+    console.log(data.current);
 });
