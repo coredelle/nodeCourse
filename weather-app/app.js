b84954e37831642c065adc23da853fc73bb638df
coredelle <westga09@gmail.com>
2020-05-12 08:50:34 -0400

    properly log lat and long

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,17 +4,17 @@ const url = "http://api.weatherstack.com/current?access_key=852f24ff0f88bb9de21b
 
 const geocodeUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY29yZWRlbGxlIiwiYSI6ImNrYTM1a3l5ajA3Zzkzbmp2ZzBzeGZhaTYifQ.yNxlyWa95Kh-426bE0GMrA&limit=1"
 
-// request({url, json: true}, (err, resp) => {
-//     // console.log(resp.body.current);
-//     const degrees = resp.body.current.temperature;
-//     const rain = resp.body.current.precip;
-//     const forecast = resp.body.current.weather_descriptions[0].toLowerCase();
-//     console.log(`It's ${forecast} and is currently ${degrees} degrees out. There is ${rain}% chance of rain!`)
-// });
+request({url, json: true}, (err, resp) => {
+    // console.log(resp.body.current);
+    const degrees = resp.body.current.temperature;
+    const rain = resp.body.current.precip;
+    const forecast = resp.body.current.weather_descriptions[0].toLowerCase();
+    console.log(`It's ${forecast} and is currently ${degrees} degrees out. There is ${rain}% chance of rain!`)
+});
 
 request({url: geocodeUrl, json: true}, (err, resp) => {
     // console.log(resp.body);
-    const lat = resp.body.features[0].center[0];
-    const long = resp.body.features[0].center[1];
+    const lat = resp.body.features[0].center[1];
+    const long = resp.body.features[0].center[0];
     console.log(`lat ${lat} long ${long}`)
 });
